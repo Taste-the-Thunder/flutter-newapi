c167c6b7b9b4e72f16ef979dabd9752a7dc653e6
dhruvinprajapati <35141691+dhruvinprajapati@users.noreply.github.com>
2023-07-26 11:29:38 +0530

    create apiservice file and api calling function

--- lib/services/ApiService.dart
+++ lib/services/ApiService.dart
@@ -1,12 +1,22 @@
 
+import 'dart:convert';
+
 import 'package:http/http.dart';
 import 'package:newsapi/Model/Article.dart';
 
 class ApiService{
 
-  final endPoint = "https://newsapi.org/v2/everything?q=tesla&from=2023-06-25&sortBy=publishedAt&apiKey=27aafa002a324c3fb9d80fe2f2c69977";
+  final endPoint = "https://newsapi.org/v2/everything?q=tesla&from=2023-06-26&sortBy=publishedAt&apiKey=27aafa002a324c3fb9d80fe2f2c69977";
 
   Future<List<Article>> getArticles() async {
-      Response response = await get(endPoint);
+      Response response = await get(Uri.parse(endPoint));
+      if(response.statusCode == 200){
+        Map<String, dynamic> body = jsonDecode(response.body);
+        List<dynamic> article = body['articles'];
+        List<Article> data = article.map((dynamic item) => Article.fromJson(item)).toList();
+        return data;
+      } else {
+        throw "Something Wrong";
+      }
   }
 }
